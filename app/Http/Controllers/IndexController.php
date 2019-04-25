5c98cbae0b990dae0eef72979827801108c063b9
game <33078552+liulixia123@users.noreply.github.com>
2019-04-25 14:02:06 +0800

    Create IndexController.php
    添加前台首页

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 前台首页控制器
+ *
+ * @author      fzs
+ * @Time: 2017/07/14 15:57
+ * @version     1.0 版本号
+ */
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+	/**
+	 * [index 首页]
+	 * @return [type] [description]
+	 */
+	public function index(){
+		return "hello world";
+	}
+}
