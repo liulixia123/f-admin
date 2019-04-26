bf0d805ebf0537df26a4c9b539b1c61f15e50669
lixia <1518545702@qq.com>
2019-04-26 10:33:17 +0800

    修改手动数据库迁移字符长度

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //手动配置迁移命令migrate生成的默认字符串长度
+        Schema::defaultStringLength(191);
     }
 
     /**
