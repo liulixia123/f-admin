f205971f228e7412dcf24ee0e8090498ee5675d4
lixia <1518545702@qq.com>
2019-05-16 09:51:20 +0800

    update

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -90,59 +90,126 @@ function errorLog($message,$file)
 
     }
 //获取浏览器信息
-function getBrowser(){
-    $flag=$_SERVER['HTTP_USER_AGENT'];
-    $para=array();
-    
-    // 检查操作系统
-    if(preg_match('/Windows[\d\. \w]*/',$flag, $match)) $para['os']=$match[0];
-    
-    if(preg_match('/Chrome\/[\d\.\w]*/',$flag, $match)){
-        // 检查Chrome
-        $para['browser']=$match[0];
-    }elseif(preg_match('/Safari\/[\d\.\w]*/',$flag, $match)){
-        // 检查Safari
-        $para['browser']=$match[0];
-    }elseif(preg_match('/MSIE [\d\.\w]*/',$flag, $match)){
-        // IE
-        $para['browser']=$match[0];
-    }elseif(preg_match('/Opera\/[\d\.\w]*/',$flag, $match)){
-        // opera
-        $para['browser']=$match[0];
-    }elseif(preg_match('/Firefox\/[\d\.\w]*/',$flag, $match)){
-        // Firefox
-        $para['browser']=$match[0];
-    }elseif(preg_match('/OmniWeb\/(v*)([^\s|;]+)/i',$flag, $match)){
-        //OmniWeb
-        $para['browser']=$match[2];
-    }elseif(preg_match('/Netscape([\d]*)\/([^\s]+)/i',$flag, $match)){
-        //Netscape
-        $para['browser']=$match[2];
-    }elseif(preg_match('/Lynx\/([^\s]+)/i',$flag, $match)){
-        //Lynx
-        $para['browser']=$match[1];
-    }elseif(preg_match('/360SE/i',$flag, $match)){
-        //360SE
-        $para['browser']='360安全浏览器';
-    }elseif(preg_match('/SE 2.x/i',$flag, $match)) {
-        //搜狗
-        $para['browser']='搜狗浏览器';
-    }elseif(preg_match('/like mac os x/i',$flag, $match)) {
-        //IOS终端
-        $para['browser']='ios终端';
-    }elseif(preg_match('/iPhone/i',$flag, $match)) {
-        //iPhone
-        $para['browser']='iPhone';
-    }elseif(preg_match('/MicroMessenger/i',$flag, $match)) {
-        //微信
-        $para['browser']='weixin';
-    }elseif(preg_match('/\sQQ/i',$flag, $match)) {
-        //QQ
-        $para['browser']='QQ';
-    }else{
-        $para['browser']='unkown';
+function get_broswer()
+{
+    $sys = $_SERVER['HTTP_USER_AGENT'];  //获取用户代理字符串
+    if (stripos($sys, "Firefox/") > 0) {
+        preg_match("/Firefox\/([^;)]+)+/i", $sys, $b);
+        $exp[0] = "Firefox";
+        $exp[1] = $b[1];  //获取火狐浏览器的版本号
+    } elseif (stripos($sys, "Maxthon") > 0) {
+        preg_match("/Maxthon\/([\d\.]+)/", $sys, $aoyou);
+        $exp[0] = "傲游";
+        $exp[1] = $aoyou[1];
+    } elseif (stripos($sys, "MSIE") > 0) {
+        preg_match("/MSIE\s+([^;)]+)+/i", $sys, $ie);
+        $exp[0] = "IE";
+        $exp[1] = $ie[1];  //获取IE的版本号
+    } elseif (stripos($sys, "OPR") > 0) {
+        preg_match("/OPR\/([\d\.]+)/", $sys, $opera);
+        $exp[0] = "Opera";
+        $exp[1] = $opera[1];
+    } elseif (stripos($sys, "Edge") > 0) {
+        //win10 Edge浏览器 添加了chrome内核标记 在判断Chrome之前匹配
+        preg_match("/Edge\/([\d\.]+)/", $sys, $Edge);
+        $exp[0] = "Edge";
+        $exp[1] = $Edge[1];
+    } elseif (stripos($sys, "Chrome") > 0) {
+        preg_match("/Chrome\/([\d\.]+)/", $sys, $google);
+        $exp[0] = "Chrome";
+        $exp[1] = $google[1];  //获取google chrome的版本号
+    } elseif (stripos($sys, 'rv:') > 0 && stripos($sys, 'Gecko') > 0) {
+        preg_match("/rv:([\d\.]+)/", $sys, $IE);
+        $exp[0] = "IE";
+        $exp[1] = $IE[1];
+    } elseif (stripos($sys, "MicroMessenger") > 0) {
+        $exp[0] = "weixin";
+        $exp[1] = "";
+    }elseif (stripos($sys, "QQ") > 0) {
+        $exp[0] = "QQ";
+        $exp[1] = "";
+    }else {
+        $exp[0] = "未知浏览器";
+        $exp[1] = "";
     }
-    return $para;
+    return $exp[0] . '(' . $exp[1] . ')';
+}
+
+//获取终端操作系统信息
+function get_os()
+{
+    $agent = $_SERVER['HTTP_USER_AGENT'];
+    $os = false;
+
+    if (preg_match('/win/i', $agent) && strpos($agent, '95')) {
+        $os = 'Windows 95';
+    } else if (preg_match('/win 9x/i', $agent) && strpos($agent, '4.90')) {
+        $os = 'Windows ME';
+    } else if (preg_match('/win/i', $agent) && preg_match('/98/i', $agent)) {
+        $os = 'Windows 98';
+    } else if (preg_match('/win/i', $agent) && preg_match('/nt 6.0/i', $agent)) {
+        $os = 'Windows Vista';
+    } else if (preg_match('/win/i', $agent) && preg_match('/nt 6.1/i', $agent)) {
+        $os = 'Windows 7';
+    } else if (preg_match('/win/i', $agent) && preg_match('/nt 6.2/i', $agent)) {
+        $os = 'Windows 8';
+    } else if (preg_match('/win/i', $agent) && preg_match('/nt 10.0/i', $agent)) {
+        $os = 'Windows 10';#添加win10判断
+    } else if (preg_match('/win/i', $agent) && preg_match('/nt 5.1/i', $agent)) {
+        $os = 'Windows XP';
+    } else if (preg_match('/win/i', $agent) && preg_match('/nt 5/i', $agent)) {
+        $os = 'Windows 2000';
+    } else if (preg_match('/win/i', $agent) && preg_match('/nt/i', $agent)) {
+        $os = 'Windows NT';
+    } else if (preg_match('/win/i', $agent) && preg_match('/32/i', $agent)) {
+        $os = 'Windows 32';
+    } else if (preg_match('/linux/i', $agent)) {
+        $os = 'Linux';
+    } else if (preg_match('/unix/i', $agent)) {
+        $os = 'Unix';
+    } else if (preg_match('/sun/i', $agent) && preg_match('/os/i', $agent)) {
+        $os = 'SunOS';
+    } else if (preg_match('/ibm/i', $agent) && preg_match('/os/i', $agent)) {
+        $os = 'IBM OS/2';
+    } else if (preg_match('/Mac/i', $agent) && preg_match('/PC/i', $agent)) {
+        $os = 'Macintosh';
+    } else if (preg_match('/PowerPC/i', $agent)) {
+        $os = 'PowerPC';
+    } else if (preg_match('/AIX/i', $agent)) {
+        $os = 'AIX';
+    } else if (preg_match('/HPUX/i', $agent)) {
+        $os = 'HPUX';
+    } else if (preg_match('/NetBSD/i', $agent)) {
+        $os = 'NetBSD';
+    } else if (preg_match('/BSD/i', $agent)) {
+        $os = 'BSD';
+    } else if (preg_match('/OSF1/i', $agent)) {
+        $os = 'OSF1';
+    } else if (preg_match('/IRIX/i', $agent)) {
+        $os = 'IRIX';
+    } else if (preg_match('/FreeBSD/i', $agent)) {
+        $os = 'FreeBSD';
+    } else if (preg_match('/teleport/i', $agent)) {
+        $os = 'teleport';
+    } else if (preg_match('/flashget/i', $agent)) {
+        $os = 'flashget';
+    } else if (preg_match('/webzip/i', $agent)) {
+        $os = 'webzip';
+    } else if (preg_match('/offline/i', $agent)) {
+        $os = 'offline';
+    } else if (strpos($agent, 'Android') !== false) {
+        preg_match("/(?<=Android )[\d\.]{1,}/", $agent, $version);
+        $os = 'Android'.$version[0];
+    } elseif (strpos($agent, 'iPhone') !== false) {
+        preg_match("/(?<=CPU iPhone OS )[\d\_]{1,}/", $agent, $version);
+        $os = 'iPhone '.str_replace('_', '.', $version[0]);
+    } elseif (strpos($agent, 'iPad') !== false) {
+        preg_match("/(?<=CPU OS )[\d\_]{1,}/", $agent, $version);
+        $os = 'iPad'.str_replace('_', '.', $version[0]);
+    } else {
+        $os = '未知操作系统';
+    }
+    return $os;
 }
 
 /**
