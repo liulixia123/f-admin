8dc23990c3607ce8f8a556ec053718b5adfb731c
lixia <1518545702@qq.com>
2019-10-10 14:54:01 +0800

    update

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -18,22 +18,34 @@ if (!function_exists('is_config_id')) {
         return in_array($id, $ids);
     }
 }
+/**
+ * [generateOperation 根据菜单生成路由，控制器，模型，模板等]
+ * @param  [string] $name [菜单名]
+ * @return [bool]       [bool]
+ */
 function generateOperation($name){
     //路由配置文件追加路由信息
     $file_path = __DIR__."/../../routes/web.php";
     $str = file_get_contents($file_path);
     if(strpos($str,$name)===false){
-        $str = str_replace("SitesController');","SitesController');\r\n\tRoute::resource('".$name."',     '".$name."Controller');",$str);
-        file_put_contents($file_path,$str);
-        //生成控制器
-        /*php artisan make:controller UserController
-        php artisan make:model User
-        php artisan make:view pages.index*/
-        $exitcode = Artisan::call('make:controller UserController');
+        if(substr_count($name,'/')==1){
+            $controllername = substr($name,1);
+            $str = str_replace("SitesController');","SitesController');\r\n\tRoute::resource('".$name."',     '".$controllername."Controller');",$str);
+            file_put_contents($file_path,$str);
+            //生成控制器
+            /*php artisan make:controller UserController --resource
+            php artisan make:model User
+            php artisan make:view pages.index*/
+            $exitcode = Artisan::call('make:controller '.$controllername.'Controller --resource');
+            //exec('php artisan make:controller '.$controllername.'Controller --resource');
+            return true;
+        }else{
+            return false;
+        }
     }else{
         return false;
     }
-    
+    return false;    
 }
 /**
 * 验证容量是否规范
